320f0a23930c9ff602d3e4fb4efc20b37815c333
zhenjiang <zhenjiang_li.iot@outlook.com>
2026-02-05 23:32:41 +0800

    chore(apps): tetris-web init

---
+++ apps/tetris-web/src/store/index.ts
@@ -0,0 +1,12 @@
+// 总仓库
+import { configureStore } from '@reduxjs/toolkit';
+// 引入主题换肤store分库
+import themeReducer from './modules/theme';
+
+export const store = configureStore({
+  reducer: {
+    // 主题换肤store分库
+    theme: themeReducer,
+    // 可以根据需要在这里继续追加其他分库
+  },
+});
--- apps/tetris-web/vite.config.ts
+++ apps/tetris-web/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react({
       babel: {
@@ -10,4 +12,19 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 5140, // 指定dev sever的端口号，默认为5173
+    host: '0.0.0.0', // 监听所有IP地址
+    // 自动打开浏览器运行以下路径的页面
+    open: '/',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist', // 指定打包输出目录，默认为dist
+    assetsDir: 'assets', // 指定静态资源输出目录，默认为assets
+  },
 });
