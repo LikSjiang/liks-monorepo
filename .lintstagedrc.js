ee8cfd48a9cc5a334ba6a1c4f6c6a1647760c48e
zhenjiang <zhenjiang_li.iot@outlook.com>
2026-02-05 22:45:17 +0800

    chore(root): 拼写字典配置、检查文件配置修改

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 export default {
   '*.{js,jsx,ts,tsx,vue,html,css,less,scss,md}': ['cspell lint'],
-  '*.{js,jsx,ts,tsx,vue,md}': ['prettier --write', 'eslint'],
+  '*.{js,jsx,ts,tsx,vue,md}': ['prettier --write', 'eslint --fix --quiet'],
 };
