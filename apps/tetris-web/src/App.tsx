0b61c48428982c40c44c88d4d98bbc73d07cdd80
zhenjiang <zhenjiang_li.iot@outlook.com>
2026-02-06 22:26:45 +0800

    refactor(apps): 配置文件更新

--- apps/tetris-web/src/App.tsx
+++ apps/tetris-web/src/App.tsx
@@ -1,12 +1,11 @@
 // import { useState } from 'react';
 import '@/common/styles/frame.less';
+import Layout from './components/layout';
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="app-container">你好</div>
+      <Layout />
     </>
   );
 }
---
+++ apps/tetris-web/src/components/layout.tsx
@@ -0,0 +1,15 @@
+import './layout.less';
+
+function Layout() {
+  return (
+    <div className="layout-container">
+      <div className="main-wrapper">
+        <div className="main"></div>
+      </div>
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
+  );
+}
+
+export default Layout;
